00033f2afd74eee8130565c7dcd78a964111b4fe
Chhotu kumar <168346582+Chhotu2003@users.noreply.github.com>
2025-07-21 14:48:41 +0000

    Greatest Common Divisor of Using  Recursion in java

---
+++ GCD.java
@@ -0,0 +1,16 @@
+public class GCD {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 12;
+        System.out.println("Greatest Common Divisor: "+GcdRecursion(num1, num2));
+
+    }
+    public static int GcdRecursion(int a,int b) {
+        // base case : if b become 0 , return a in GCD;
+        if(b==0) return a;
+        else return GcdRecursion(b,a%b);
+    }
+}
+
+///////////// and ////////////////////////
+/// out = 4;
